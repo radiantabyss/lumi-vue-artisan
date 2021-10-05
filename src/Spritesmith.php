cdb9ccacc15d4a99fb774dc77f5f1299a1aafdf4
Robert Buchi <robert@planetweb.ro>
2021-10-05 18:41:31 +0300

    spritesmith change

--- src/Spritesmith.php
+++ src/Spritesmith.php
@@ -15,17 +15,17 @@ class Spritesmith
     private static function svg() {
         exec('npx svg-sprite-generate -d sprites/svgs -o sprites.svg');
 
-        $vue_component = '<template>
-        <svg xmlns="http://www.w3.org/2000/svg" style="position: absolute; width: 0; height: 0; overflow: hidden;">
+        $vue_component = '<script>
+export default {
+    name: \'SpritesComponent\'
+}
+</script>
 
-        </svg>
-        </template>
+<template>
+<svg xmlns="http://www.w3.org/2000/svg" style="position: absolute; width: 0; height: 0; overflow: hidden;">
 
-        <script>
-        export default {
-            name: \'SvgSpritesComponent\'
-        }
-        </script>';
+</svg>
+</template>';
 
         if ( file_exists('sprites.svg') ) {
             $sprites = file_get_contents('sprites.svg');
@@ -69,7 +69,7 @@ class Spritesmith
             unlink('sprites.svg');
         }
 
-        file_put_contents('src/vues/global-components/SvgSpritesComponent.vue', $vue_component);
+        file_put_contents('src/components/SpritesComponent.vue', $vue_component);
 
     }
 }
