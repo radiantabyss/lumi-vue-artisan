f06847b536bf203cf333e7f46852117893d8e233
Robert Buchi <robert@planetweb.ro>
2023-06-20 12:05:25 +0300

    sprites location only in common

--- src/Commands/SpriteCommand.php
+++ src/Commands/SpriteCommand.php
@@ -86,13 +86,6 @@ export default {
             unlink('sprites.svg');
         }
 
-        if ( file_exists('src/Domains/Common/Components/SpritesComponent.vue') ) {
-            file_put_contents('src/Domains/Common/Components/SpritesComponent.vue', $vue_component);
-        }
-        else {
-            file_put_contents('src/components/SpritesComponent.vue', $vue_component);
-        }
-
-
+        file_put_contents('src/Domains/Common/Components/SpritesComponent.vue', $vue_component);
     }
 }
