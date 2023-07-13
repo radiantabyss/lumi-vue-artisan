42e1c2c176e9da210a99032b3bfef0fd537c02fa
Robert Buchi <robert@planetweb.ro>
2023-07-13 09:34:52 +0300

    lang

--- src/Commands/HelpCommand.php
+++ src/Commands/HelpCommand.php
@@ -6,14 +6,15 @@ class HelpCommand implements CommandInterface
     public static function run($options) {
         echo "Commands List:\n
 \033[32mbuild\033[0m                 Builds the code
-    \033[93m--fast\033[0m             \033[90mskips 'npm install' and sprites when building\033[0m
+    \033[93m--fast\033[0m            \033[90mskips 'npm install' and sprites when building\033[0m
     \033[93m--skip-sprites\033[0m
     \033[93m--skip-build\033[0m
     \033[93m--skip-publish\033[0m
     \033[93m--keep-dark-mode\033[0m
     \033[93m--version=\033[0m        Required for Electron\n
 \033[32mpublish\033[0m               Publishes the build (release or upload)
-\033[32msprite\033[0m                Builds the sprites\n\n
-\033[32mstatic\033[0m    Copies static assets to public folder\n\n";
+\033[32msprite\033[0m                Builds the sprite
+\033[32mstatic\033[0m                Copies static assets to public folder
+\033[32mlang\033[0m                  Parses translated text from src and puts them in lang/{lang}.json\n\n";
     }
 }
---
+++ src/Commands/LangCommand.php
@@ -0,0 +1,53 @@
+<?php
+namespace LumiVueArtisan\Commands;
+
+class LangCommand implements CommandInterface
+{
+    public static function run($options) {
+        $terms = self::terms();
+        $langs = self::langs();
+
+        foreach ( $langs as $lang ) {
+            $lang_contents = isset($options['force']) ? [] : decode_json(file_get_contents('static/lang/'.$lang.'.json', 'UTF-8'));
+
+            foreach ( $terms as $term ) {
+                $lang_contents[$term] = $lang_contents[$term] ?? $term;
+            }
+
+            file_put_contents('static/lang/'.$lang.'.json', json_encode($lang_contents, JSON_PRETTY_PRINT|JSON_UNESCAPED_UNICODE));
+        }
+    }
+
+    private static function terms() {
+        $files = get_files_recursive('src');
+        $terms = [];
+
+        foreach ( $files as $file ) {
+            if ( preg_match('/\.scss/', $file)  ) {
+                continue;
+            }
+
+            $contents = file_get_contents($file);
+            preg_match_all('/\_\_\(\'(.*?)\'\)/', $contents, $matches);
+
+            foreach ( $matches[1] as $match ) {
+                $terms[] = $match;
+            }
+        }
+
+        return array_unique($terms);
+    }
+
+    private static function langs() {
+        $files = scandir('static/lang');
+        $langs = [];
+
+        foreach ( $files as $file ) {
+            if ( preg_match('/\.json/', $file) ) {
+                $langs[] = str_replace('.json', '', $file);
+            }
+        }
+
+        return $langs;
+    }
+}
